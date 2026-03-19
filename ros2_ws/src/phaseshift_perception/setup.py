3a3632c6301cc9eec7cbd824f513c1002c7badb3
eun <eun1212p@gmail.com>
2026-03-19 00:32:44 +0000

    Add perception package and YOLO

---
+++ ros2_ws/src/phaseshift_perception/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'phaseshift_perception'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='eun',
+    maintainer_email='eun1212p@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'yolo_detector_node = phaseshift_perception.yolo_detector_node:main'
+        ],
+    },
+)
